7a2d2a879a449e5df64409b919a438e459ca5057
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2011-07-31 16:53:57 -0500

    Use one single bulk insert rather than many individual inserts. Closes #1.

--- crumbs-worker.js
+++ crumbs-worker.js
@@ -1,27 +1,25 @@
 onmessage = function(message) {
-    var completed = 0;
-    var rows = message.data.data.split('\n');
-    rows.forEach(function(row) {
+    var docs = [];
+    var rows = message.data.data.split('\n').filter(function(field) {
         // Skip empty rows
-        if (row.length == 0) {
-            completed++;
-        } else {
-            var doc = {};
-            row.split(',').forEach(function(field, index) {
-                doc['field' + index] = field;
-            });
-            var req = new XMLHttpRequest();
-            req.onreadystatechange = function() {
-                if (req.readyState == 4) {
-                    completed++;
-                    if (completed == rows.length) {
-                        postMessage('Dump complete.');
-                    }
-                }
-            };
-            req.open('POST', message.data.url);
-            req.setRequestHeader('Content-Type', 'application/json');
-            req.send(JSON.stringify(doc));
+        if (field.length > 0) {
+            return field;
         }
+    }).forEach(function(row) {
+        var doc = {};
+        row.split(',').forEach(function(field, index) {
+            doc['field' + index] = field;
+        });
+        docs.push(doc);
     });
+    var req = new XMLHttpRequest();
+    var body = {'docs' : docs};
+    req.onreadystatechange = function() {
+        if (req.readyState == 4) {
+            postMessage('Dump complete.');
+        }
+    };
+    req.open('POST', message.data.url + '_bulk_docs');
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.send(JSON.stringify(body));
 };
