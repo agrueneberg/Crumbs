8d6aeabf22431f422100db74981dfcc86727d341
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2011-11-29 10:08:06 -0600

    Added drag-and-drop import.

--- _attachments/scripts/crumbs-worker.js
+++ _attachments/scripts/crumbs-worker.js
@@ -1,34 +1,42 @@
-onmessage = function(message) {
-    var docs = [];
-    var fields = [];
-    var rows = message.data.data.split('\n').filter(function(field) {
-        // Skip empty rows
+onmessage = function (message) {
+    var docs, fields, delimiter, rows, req, url, body;
+    docs = [];
+    fields = [];
+    delimiter = message.data.options.delimiter || ",";
+    rows = message.data.data;
+    rows = rows.split(/\r?\n/).filter(function (field) {
+        // Skip empty rows.
         if (field.length > 0) {
             return field;
         }
     });
-    if (message.data.options && message.data.options.firstLineHasFieldNames) {
-        fields = rows.shift().split(',');
+    if (message.data.options.firstLineHasFieldNames === true) {
+        // Populate field names from first row.
+        fields = rows.shift().split(delimiter);
     }
-    rows.forEach(function(row) {
-        var doc = {};
-        row.split(',').forEach(function(field, index) {
-            var field_name = fields[index] || 'field' + index;
+    rows.forEach(function (row) {
+        var doc;
+        doc = {};
+        row.split(delimiter).forEach(function (field, index) {
+            var field_name;
+            field_name = fields[index] || "field" + index;
             doc[field_name] = field;
         });
         docs.push(doc);
     });
     if (docs.length > 0) {
-        var req = new XMLHttpRequest();
-        var url = location.protocol + '//' + location.host + '/' + location.pathname.split('/')[1] + '/';
-        var body = {'docs' : docs};
-        req.onreadystatechange = function() {
+        req = new XMLHttpRequest();
+        url = location.protocol + "//" + location.host + "/" + location.pathname.split("/")[1] + "/";
+        body = {
+            "docs" : docs
+        };
+        req.onreadystatechange = function () {
             if (req.readyState == 4) {
                 postMessage(0);
             }
         };
-        req.open('POST', url + '_bulk_docs');
-        req.setRequestHeader('Content-Type', 'application/json');
+        req.open("POST", url + "_bulk_docs");
+        req.setRequestHeader("Content-Type", "application/json");
         req.send(JSON.stringify(body));
     } else {
         postMessage(1);
---
+++ _attachments/scripts/crumbs.js
@@ -0,0 +1,60 @@
+// jQuery creates it's own event object, and it doesn't have a
+// dataTransfer property yet. This adds dataTransfer to the event object.
+$.event.props.push("dataTransfer");
+$(function() {
+    var dump;
+    dump = function (file) {
+        var spinner, worker, reader, optionFirstLineHasFieldNames, optionDelimiter;
+        spinner = $("#spinner");
+        spinner.show();
+        if (file) {
+            worker = new Worker("scripts/crumbs-worker.js");
+            optionFirstLineHasFieldNames = $("#optionFirstLineHasFieldNames").is(":checked");
+            optionDelimiter = $("#optionDelimiter").val();
+            reader = new FileReader();
+            reader.onload = function (event) {
+                var payload;
+                payload = {
+                    data: event.target.result,
+                    options: {
+                        firstLineHasFieldNames: optionFirstLineHasFieldNames,
+                        delimiter: optionDelimiter
+                    }
+                };
+                worker.onmessage = function (message) {
+                    if (message.data === 0) {
+                        alert("Import complete.");
+                    } else {
+                        alert("Something went wrong.");
+                    }
+                    spinner.hide();
+                };
+                worker.postMessage(payload);
+            };
+            reader.readAsText(file);
+        } else {
+            spinner.hide();
+            alert("Please provide a CSV file.");
+        }
+    };
+    // Button handlers.
+    $("#button").click(function () {
+        file = $("#picker").get(0).files[0];
+        dump(file);
+    });
+    // Drag-and-Drop handlers.
+    $("#dropzone").bind("dragover", function (e) {
+        e.preventDefault();
+        $(this).css("background", "red");
+    });
+    $("#dropzone").bind("dragleave", function (e) {
+        e.preventDefault();
+        $(this).css("background", "white");
+    });
+    $("#dropzone").bind("drop", function (e) {
+        e.preventDefault();
+        $(this).css("background", "white");
+        file = e.dataTransfer.files[0];
+        dump(file);
+    });
+});
